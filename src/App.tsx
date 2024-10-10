5d62b117dade80ab9efaf1fad5abe98b3181619c
Richard Da Silva <daniel34dasilva@gmail.com>
2024-10-10 10:08:19 -0400

    Update main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Github, Linkedin, Mail, ExternalLink, Code, Briefcase, User, ChevronDown } from 'lucide-react'
+import { Github, Linkedin, Mail, ExternalLink, ChevronDown } from 'lucide-react'
 
 const projects = [
   {
@@ -15,7 +15,7 @@ const projects = [
   {
     title: "Green STF",
     description: "Pagina web sencilla para mostrar la finalidad de la empresa.",
-    link: "https://greenstf.netlify.app/"
+    link: "https://greenstf.es/"
   },
   {
     title: "Formulario de Proyecto de Free Code Camp",
@@ -23,7 +23,7 @@ const projects = [
     link: "https://projectfreecodecamp.netlify.app/"
   },
   {
-    title: "Trascendence",
+    title: "Trascendence V1",
     description: "Pagina web sencilla para mostrar la finalidad de la empresa.",
     link: "https://trascendence.netlify.app/"
   },
@@ -34,12 +34,12 @@ const projects = [
   },
   {
     title: "Focaliza 2030",
-    description: "Pagina web creada con Wordpress, se usó Elementhor y codigo. Empresa española de Outsourcing comercial.",
+    description: "Pagina web creada con Wordpress, se usó Elementor y codigo. Empresa española de Outsourcing comercial.",
     link: "https://trascendence.es/"
   },
   {
     title: "Nebula Gin",
-    description: "Pagina web creada con Wordpress, se usó Elementhor y codigo. Pagina principal para mostrar producto Nebula Gin.",
+    description: "Pagina web creada con Wordpress, se usó Elementor y codigo. Pagina principal para mostrar producto Nebula Gin.",
     link: "https://nebulagin.com/"
   },
   {
@@ -54,22 +54,22 @@ const projects = [
   },
   {
     title: "Gin Cat",
-    description: "Pagina web creada con Wordpress, se usó Elementhor y codigo. Pagina principal para mostrar producto Gin Cat.",
+    description: "Pagina web creada con Wordpress, se usó Elementor y codigo. Pagina principal para mostrar producto Gin Cat.",
     link: "https://gincat.cat/"
   },
   {
     title: "Alissa TV",
-    description: "Pagina web creada con Wordpress, se usó Elementhor y codigo. Pagina principal para llevar a perfil de YouTube (Creado y Gestionado por mi)",
+    description: "Pagina web creada con Wordpress, se usó Elementor y codigo. Pagina principal para llevar a perfil de YouTube (Creado y Gestionado por mi)",
     link: "https://alissatv.com/"
   },
   {
     title: "Consumo Estrategico",
-    description: "Pagina web creada con Wordpress, se usó Elementhor y codigo. Pagina principal de Blog sobre acontecimientos e información de interes.",
+    description: "Pagina web creada con Wordpress, se usó Elementor y codigo. Pagina principal de Blog sobre acontecimientos e información de interes.",
     link: "https://consumoestrategico.com/"
   }
 ]
 
-const skills = ["React", "TypeScript", "Node.js", "Javascript", "SQL", "CSS", "HTML", "Python", "Git", "Github", "Tailwind CSS", "Wordpress", "Elementhor", "Illustrator", "PhotoShop", "Adobe Premiere", "Excel Avanzado", "Premiere Pro", "After effects", "Davinci Resolve"  ]
+const skills = ["React", "TypeScript", "Node.js", "Javascript", "SQL", "CSS", "HTML", "Python", "Git", "Github", "Tailwind CSS", "Wordpress", "Figma", "Elementor", "Illustrator", "PhotoShop", "Adobe Premiere", "Excel Avanzado", "Premiere Pro", "After effects", "Davinci Resolve"]
 
 function App() {
   return (
@@ -80,7 +80,7 @@ function App() {
           <a href="#" className="text-2xl font-bold">RD</a>
           <div className="flex space-x-4">
             <a href="#about" className="hover:text-indigo-200">Sobre mí</a>
-            <a href="#projects" className="hover:text-indigo-200">Proyecto</a>
+            <a href="#projects" className="hover:text-indigo-200 ">Proyecto</a>
             <a href="#contact" className="hover:text-indigo-200">Contacto</a>
           </div>
         </nav>
@@ -91,34 +91,44 @@ function App() {
         <div className="container mx-auto px-6 text-center">
           <h1 className="text-5xl font-bold mb-4">Richard Da Silva</h1>
           <p className="text-xl mb-8">Full Stack Developer | Python Specialist | Problem Solver</p>
-          <a href="mailto:daniel34dasilva@gmail.com" className="bg-white text-indigo-700 px-6 py-3 rounded-full font-bold hover:bg-indigo-100 transition duration-300">Contactame</a>
+          <a href="mailto:daniel34dasilva@gmail.com" className="bg-white text-neutral-700 px-6 py-3 rounded-full font-bold hover:bg-indigo-100 transition duration-300">Contactame</a>
         </div>
-        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2">
-          <ChevronDown className="animate-bounce w-8 h-8 text-white" />
-        </div>
+
       </section>
 
       {/* About Section */}
       <section id="about" className="py-20">
         <div className="container mx-auto px-6">
-        <h2 className="text-3xl font-bold mb-8 text-center" id="about">Sobre mí</h2>
+          <h2 className="text-3xl font-bold mb-8 text-center" id="about">Sobre mí</h2>
           <div className="flex flex-col md:flex-row items-center justify-between">
             <div className="md:w-1/2 mb-8 md:mb-0">
-              <img src="https://avatars.githubusercontent.com/u/26756784?v=4" alt="Richard Da Silva" className="rounded-full w-64 h-64 object-cover mx-auto" />
+              <img src="https://avatars.githubusercontent.com/u/26756784?v=4" alt="Richard Da Silva" className="rounded-full w-94 h-94 object-cover mx-auto" />
             </div>
             <div className="md:w-1/2">
               <p className="text-lg mb-4">
-               ¡Hola! Mi nombre es Richard Da Silva, Programador Jr
+                ¡Hola! Mi nombre es Richard Da Silva, Programador Jr.
               </p>
               <p className="text-lg mb-6">
-              Desarrollador web proactivo con una sólida base en tecnologías web modernas. Me apasiona encontrar soluciones creativas a desafíos complejos utilizando herramientas como React, TypeScript, Node.js y SQL. Soy un aprendiz constante, siempre dispuesto a explorar nuevas tecnologías y mejorar mis habilidades
+                Desarrollador web proactivo con una sólida base en tecnologías web modernas. Me apasiona encontrar soluciones creativas a desafíos complejos utilizando herramientas como React, TypeScript, Node.js y SQL. Soy un aprendiz constante, siempre dispuesto a explorar nuevas tecnologías y mejorar mis habilidades.
+              </p>
+              <p className="text-lg mb-6">
+                Cuento con experiencia en Excel, trabaje en Mostrar, Filtrar y Desarrollar archivos para ver sus resultados como Tablas dinamicas, creando informes didacticos.
+
+              </p>
+              <p className="text-lg mb-6">
+                Poseo experiencia demostrale en el uso de programas de diseño graficos, tales como Photoshop, Illustrator, Premiere Pro, After Effects y Davinci Resolve.
+              </p>
+              <p className="text-lg mb-6">
+                Lenguaje de programacion Preferido: "Python" por excelencia, divertido y muy dinamico.
               </p>
               <div className="flex flex-wrap gap-2">
                 {skills.map((skill, index) => (
                   <span key={index} className="bg-indigo-100 text-indigo-800 px-3 py-1 rounded-full text-sm">{skill}</span>
                 ))}
               </div>
+
             </div>
+
           </div>
         </div>
       </section>
@@ -144,7 +154,7 @@ function App() {
       </section>
 
       {/* Footer */}
-      <footer  className="bg-neutral-800 text-white py-8">
+      <footer className="bg-neutral-800 text-white py-8">
         <div className="container mx-auto px-6" id="contact">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="mb-4 md:mb-0">
